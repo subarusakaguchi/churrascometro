71bb5a349f41559bddc9526ad5677f9730674edb
Subaru Sakaguchi <subarusakaguchi@yahoo.com.br>
2021-06-04 13:20:50 -0300

    Funcionalmente pronto

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function calcular() {
+    let inputAdulto = document.getElementById('adulto')
+    let inputCrianca = document.getElementById('crianca')
+    let inputHora = document.getElementById('hora')
+    if (inputAdulto.value.length === 0 || inputCrianca.value.length === 0 || inputHora.value.length === 0) {
+        alert('ERRO! você esqueceu algum dos valores, por favor preencha todos os dados!')
+    } else {
+        let res = document.getElementById('res')
+        let adulto = Number(inputAdulto.value)
+        let crianca = Number(inputCrianca.value)
+        let hora = Number(inputHora.value)
+        let resCarne, resCerva, resRefri
+
+        if (hora <= 6) {
+            resCarne = (0.4 * adulto) + (0.2 * crianca)
+            resCerva = 1.2/0.355 * adulto
+            resRefri = (adulto + (0.5 * crianca)) / 2
+        } else {
+            resCarne = (0.65 * adulto) + (0.325 * crianca)
+            resCerva = 2/0.35 * adulto
+            resRefri = ((1.5 * adulto) + (0.75 * crianca)) / 2
+        }
+
+        res.innerText = ''
+        res.innerText += 'Serão necessários:\n'
+        res.innerText += `${resCarne.toFixed(2)}Kg de carne\n`
+        res.innerText += `${Math.ceil(resCerva)} latas de cerveja\n`
+        res.innerText += `${Math.ceil(resRefri)} garrafas de refrigerante\n`
+    } 
+}
